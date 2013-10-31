5148321c0fe2d404982437658208f5ace756c749
d0ge01 <not.d0ge@anche.no>
2013-10-31 01:31:56 +0100

    started...

--- src/it/d0ge01/karma/lib/Lib.java
+++ src/it/d0ge01/karma/lib/Lib.java
@@ -1,5 +1,9 @@
 package it.d0ge01.karma.lib;
 
 public abstract class Lib {
-	private String aircrackBin;
+	private String aircrackBin;		// Aircrack-ng path
+	private String apbin;			// Airbase-ng path
+	private String wifiInterface;	// Wireless Interface
+	private String ethInterface;	// Ethernet Interface
+	
 }
---
+++ src/it/d0ge01/karma/util/Utils.java
@@ -0,0 +1,5 @@
+package it.d0ge01.karma.util;
+
+public class Utils {
+
+}
